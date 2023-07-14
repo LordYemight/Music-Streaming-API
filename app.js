8fc8927e475d45b22e37300387d3792a6bc934b6
Okunola Olayemi <Okunola.O@bullseye.local>
2023-07-14 08:54:30 +0100

    another commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+require('dotenv/config');
+const mongoose = require('./db/db');
+const router = require('./routes/router');
+const port = process.env.port
+
+
+
+app.use (express.json());
+app.use ('/', router);
+
+app.listen (port , async () => {
+  await mongoose;
+  console.log(`Server listening on ${port}`)
+})
